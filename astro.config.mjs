687145dcd7f94158b6380a8925964bd8b5489ee9
Tal cohen <talcohen@Tals-MacBook-Pro.local>
2026-06-14 17:09:15 +1000

    Remove sitemap to fix CI build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,21 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-
 export default defineConfig({
   site: 'https://tal-cohen.me',
+
   integrations: [
     mdx(),
     tailwind({ applyBaseStyles: false }),
-    sitemap(),
   ],
+
   build: {
     inlineStylesheets: 'auto',
   },
+
   vite: {
     build: {
       cssCodeSplit: true,
     },
-  },
-  experimental: {
-    clientPrerender: true,
   },
 });
